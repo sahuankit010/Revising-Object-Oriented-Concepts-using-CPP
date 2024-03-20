9b7e8876ce0318862255a566841a000e35a6d357
sahuankit010 <sahu.ankit010@gmail.com>
2024-03-19 22:14:17 -0500

    adding operator overloading example

--- Operator_Overloading_Complex.cpp
+++ Operator_Overloading_Complex.cpp
@@ -18,6 +18,10 @@ public:
         return Complex(this->i + c.i, this->r+c.r );
     }
 
+    Complex operator- (const Complex &c){ // copy constructor
+        return Complex(this->r - c.r, this->i - c.i);
+    }
+
     void print(){
         cout<<this->r<< " + "<< this->i<<"i"<<endl;
     }
@@ -28,7 +32,10 @@ int main(){
     Complex c2(3, 2);
     c1.print();
     c2.print();
+
     Complex c3 = c1+ c2;
+    Complex c4 = Complex(20, 20) - c3;
 
     c3.print();
+    c4.print();
 }
